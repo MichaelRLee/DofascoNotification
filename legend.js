7c430813d6cd3f1fafccc5429941975010c17d48
Michael <leemr2@mcmaster.ca>
2017-01-28 22:40:24 -0500

    added comments and fixed up padding

--- legend.js
+++ legend.js
@@ -1,33 +1,35 @@
-var cb=false;
+var cb=false; // colourblind mode
 
+//open legend
 function openLeg() {
     document.getElementById("sideLegend").style.width = "230px";
 }
 
+//close legend
 function closeLeg() {
     document.getElementById("sideLegend").style.width = "0";
 }
 
+//toggle colourblind mode
 function toggleCB(){
-	classes=["red","orange","yellow","green"]
+	classes=["red","orange","yellow","green"] //colour classes we are targetting
 	if (cb==false){
-		//colours = ["#FF0000","#939393","#cbcbcb","#e2e2e2"]
-		colours = ["rgb(0,114,178)","rgb(213,94,0)","rgb(230,159,0)","rgb(240,228,66)"]
+		colours = ["rgb(0,114,178)","rgb(213,94,0)","rgb(230,159,0)","rgb(240,228,66)"] //colourblind colours
 		for (var i = 0; i<classes.length;i++){
-			instances = document.getElementsByClassName(classes[i])
+			instances = document.getElementsByClassName(classes[i]) //get list of colour class instances
 			for (var j = 0; j < instances.length; j++){
-				instances[j].style.backgroundColor=colours[i];
+				instances[j].style.backgroundColor=colours[i]; //change the colour of each instance of colour class
 			}
 		}
-		cb=true;
+		cb=true; //set colourblind mode to true
 	}else {
-		colours = ["#ffb3ba","#ffdfba","#ffffba","#baffc9"]
+		colours = ["#ffb3ba","#ffdfba","#ffffba","#baffc9"] //regular colours
 		for (var i = 0; i<classes.length;i++){
-			instances = document.getElementsByClassName(classes[i])
+			instances = document.getElementsByClassName(classes[i]) //get list of colour class instances
 			for (var j = 0; j < instances.length; j++){
-				instances[j].style.backgroundColor=colours[i];
+				instances[j].style.backgroundColor=colours[i]; //change colour of each instance of colour class
 			}
 		}
-		cb=false;
+		cb=false; //set colourblind mode to false
 	}
 }
